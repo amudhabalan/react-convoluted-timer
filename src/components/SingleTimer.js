1ef718f61a05fb6be4d8ba01abd9c818c580780a
amud <amudhabalan@gmail.com>
2020-02-10 09:38:36 +1100

    Optimizes code

--- src/components/SingleTimer.js
+++ src/components/SingleTimer.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, Typography, Button } from "@material-ui/core";
 import TimeDisplay from "./TimeDisplay";
 
-const SingleTimer = ({ timer, timerIsOn, setTimer, setTimerIsOn }) => {
+const SingleTimer = ({
+  timer,
+  timerIsOn,
+  setTimer,
+  setTimerIsOn,
+  intervalTime
+}) => {
+  useEffect(() => {
+    let interval = null;
+    if (timerIsOn) {
+      interval = setInterval(() => {
+        setTimer(timer => timer + intervalTime);
+      }, intervalTime);
+    } else if (!timerIsOn && timer !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timerIsOn, timer, setTimer, intervalTime]);
+
   return (
     <Grid item xs={4} style={{ textAlign: "center" }}>
       <Typography variant="h5">Timer 3</Typography>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 import SingleTimer from "./SingleTimer";
 import TotalTimer from "./TotalTimer";
@@ -11,48 +11,12 @@ const Timer = () => {
   const [timer3, setTimer3] = useState(0);
   const [timer3IsOn, setTimer3IsOn] = useState(false);
 
-  useEffect(() => {
-    let interval1 = null;
-    if (timer1IsOn) {
-      interval1 = setInterval(() => {
-        setTimer1(timer1 => timer1 + 100);
-      }, 100);
-    } else if (!timer1IsOn && timer1 !== 0) {
-      clearInterval(interval1);
-    }
-    return () => clearInterval(interval1);
-  }, [timer1IsOn, timer1]);
-
-  useEffect(() => {
-    let interval2 = null;
-    if (timer2IsOn) {
-      interval2 = setInterval(() => {
-        setTimer2(timer2 => timer2 + 1000);
-      }, 1000);
-    } else if (!timer2IsOn && timer2 !== 0) {
-      clearInterval(interval2);
-    }
-    return () => clearInterval(interval2);
-  }, [timer2IsOn, timer2]);
-
-  useEffect(() => {
-    let interval3 = null;
-    if (timer3IsOn) {
-      interval3 = setInterval(() => {
-        setTimer3(timer3 => timer3 + 10000);
-      }, 10000);
-    } else if (!timer3IsOn && timer3 !== 0) {
-      clearInterval(interval3);
-    }
-    return () => clearInterval(interval3);
-  }, [timer3IsOn, timer3]);
-
   return (
     <Grid container spacing={5}>
       <TotalTimer timer={timer1+timer2+timer3} />
-      <SingleTimer timer={timer1} timerIsOn={timer1IsOn} setTimer={setTimer1} setTimerIsOn={setTimer1IsOn}/>
-      <SingleTimer timer={timer2} timerIsOn={timer2IsOn} setTimer={setTimer2} setTimerIsOn={setTimer2IsOn}/>
-      <SingleTimer timer={timer3} timerIsOn={timer3IsOn} setTimer={setTimer3} setTimerIsOn={setTimer3IsOn}/>
+      <SingleTimer timer={timer1} timerIsOn={timer1IsOn} setTimer={setTimer1} setTimerIsOn={setTimer1IsOn} intervalTime={100}/>
+      <SingleTimer timer={timer2} timerIsOn={timer2IsOn} setTimer={setTimer2} setTimerIsOn={setTimer2IsOn} intervalTime={1000}/>
+      <SingleTimer timer={timer3} timerIsOn={timer3IsOn} setTimer={setTimer3} setTimerIsOn={setTimer3IsOn} intervalTime={10000}/>
     </Grid>
   );
 };
